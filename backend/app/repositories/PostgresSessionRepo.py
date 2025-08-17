c8e6db8a3d67e6857b6591a4614ae119f812e5b7
Kevin Nguyen <KNguyenSky@gmail.com>
2025-08-17 01:12:06 -0400

    Feature/url session caching (#10)
    * Implemented Session Caching in Redis
    
    * Updated CD.yaml file
    
    - Also did linting and formatting
    
    * Url clicks polishing
    - Added TTL to url clicks to prevent memory leaks and bloated state
    - Remove url clicks

--- backend/app/repositories/PostgresSessionRepo.py
+++ backend/app/repositories/PostgresSessionRepo.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 import asyncpg
 from app.services.postgres import get_postgres_pool
 
@@ -7,17 +7,25 @@ class PostgresSessionRepo:
   def __init__(self, pool: asyncpg.Pool):
     self.pool = pool
 
-  async def create_session(self, user_id: str, session_token: str):
+  async def create_session(
+    self,
+    user_id: int,
+    session_token: str,
+    created_at: datetime,
+    last_active_at: datetime,
+  ):
     """Creates a session in the database"""
     async with self.pool.acquire() as conn:
       return await conn.execute(
-        """INSERT INTO sessions (user_id, session_token) VALUES ($1, $2)""",
+        """INSERT INTO sessions (user_id, session_token, created_at, last_active_at) VALUES ($1, $2, $3, $4)""",
         user_id,
         session_token,
+        created_at,
+        last_active_at,
       )
 
   async def update_session_last_active_by_user_id(
-    self, last_active_at: timedelta, user_id: str
+    self, last_active_at: timedelta, user_id: int
   ):
     """Updates when a session was last active based for a given user"""
     async with self.pool.acquire() as conn:
@@ -27,12 +35,12 @@ class PostgresSessionRepo:
         user_id,
       )
 
-  async def get_session_by_user_id(self, user_id: str):
+  async def get_session_by_user_id(self, user_id: int):
     """Returns a record of a session from the database"""
     async with self.pool.acquire() as conn:
       return await conn.fetchrow("SELECT * FROM sessions WHERE user_id = $1", user_id)
 
-  async def delete_session_by_user_id(self, user_id: str):
+  async def delete_session_by_user_id(self, user_id: int):
     """Deletes a session in the database"""
     async with self.pool.acquire() as conn:
       return await conn.execute("DELETE FROM sessions WHERE user_id = $1", user_id)
